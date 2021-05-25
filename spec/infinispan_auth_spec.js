3333af0d7b5e8ecb0be6227481fbd6409b9195a1
Katia Aresti <karesti@redhat.com>
2021-05-25 14:48:05 +0200

    attempt

--- spec/infinispan_auth_spec.js
+++ spec/infinispan_auth_spec.js
@@ -1,19 +1,19 @@
 var t = require('./utils/testing'); // Testing dependency
 
 describe('Infinispan local client', function() {
-    it('PLAIN', function(done) {t.client(t.local, {authentication: {
-            enabled: true,
-            saslMechanism: 'PLAIN',
-            userName: 'admin',
-            password: 'pass'
-        }})
-        .then(t.assert(t.put('key', 'value')))
-        .then(t.disconnect())
-        .catch(t.failed(done))
-        .finally(done);
-    });
+    // it('PLAIN', function(done) {t.client(t.local, {authentication: {
+    //         enabled: true,
+    //         saslMechanism: 'PLAIN',
+    //         userName: 'admin',
+    //         password: 'pass'
+    //     }})
+    //     .then(t.assert(t.put('key', 'value')))
+    //     .then(t.disconnect())
+    //     .catch(t.failed(done))
+    //     .finally(done);
+    // });
 
-    // it('SCRAM-SHA-512', function(done) {t.client(t.local, {authentication: {
+    // it('SCRAM-SHA-1', function(done) {t.client(t.local, {authentication: {
     //         enabled: true,
     //         saslMechanism: 'SCRAM-SHA-1',
     //         userName: 'admin',
@@ -25,16 +25,16 @@ describe('Infinispan local client', function() {
     //   .finally(done);
     // });
 
-    // it('DIGEST-MD5', function(done) {t.client(t.local, {authentication: {
-    //         enabled: true,
-    //         saslMechanism: 'DIGEST-MD5',
-    //         userName: 'admin',
-    //         password: 'pass'
-    //     }})
-    //     .then(t.assert(t.put('key', 'value')))
-    //     .then(t.disconnect())
-    //     .catch(t.failed(done))
-    //     .finally(done);
-    // });
+    it('DIGEST-MD5', function(done) {t.client(t.local, {authentication: {
+            enabled: true,
+            saslMechanism: 'DIGEST-MD5',
+            userName: 'admin',
+            password: 'pass'
+        }})
+        .then(t.assert(t.put('key', 'value')))
+        .then(t.disconnect())
+        .catch(t.failed(done))
+        .finally(done);
+    });
 
 });
